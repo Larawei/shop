c2040c72e25abe4db8d411ff132fdc6863f2b81a
LaraWei <764098204@qq.com>
2018-11-03 15:05:42 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,3 +1,9 @@
 <?php
 
 // 辅助函数 在composer.json中autoload files引入
+
+// 此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
