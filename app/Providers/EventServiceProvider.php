8b32ea8036fa060b01b9e0e9927498b9fa4f2e5b
LaraWei <764098204@qq.com>
2018-11-04 23:54:54 +0800

    触发激活邮件 And Add itsgoingd/clockwork

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Listeners\RegisteredListener;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -16,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\Event' => [
             'App\Listeners\EventListener',
         ],
+        Registered::class => [
+            RegisteredListener::class,
+        ]
     ];
 
     /**
