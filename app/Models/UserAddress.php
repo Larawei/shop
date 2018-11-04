cd14ecf96708e9cd37ffe59e2ce18c26cd4b8af0
LaraWei <764098204@qq.com>
2018-11-05 00:34:20 +0800

    新增收货地址

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    protected $fillable = [
+        'province',
+        'city',
+        'district',
+        'address',
+        'zip',
+        'contact_name',
+        'contact_phone',
+        'last_used_at',
+    ];
+
+    // 表示 last_used_at 是一个日期字段 (Carbon对象)
+    protected $dates = ['last_used_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFullAddressAttribute()
+    {
+        return "{$this->province}{$this->city}{$this->district}{$this->address}";
+    }
+}
