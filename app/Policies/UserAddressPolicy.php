e5700bf15eb91652299f5f32c8c49ab2c38aad6e
LaraWei <764098204@qq.com>
2018-11-05 00:47:37 +0800

    修改和删除收货地址

---
+++ app/Policies/UserAddressPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\UserAddress;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserAddressPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function own(User $user, UserAddress $address)
+    {
+        return $user->id === $address->user_id;
+    }
+}
