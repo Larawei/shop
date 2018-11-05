4f109fad8cf8dd33a0ef8c42cf3194134058f08c
LaraWei <764098204@qq.com>
2018-11-05 13:37:38 +0800

    管理后台 - 用户列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class UsersController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('用户列表')
+            ->description('管理用户列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        $grid->id('Id')->sortable();
+        $grid->name('用户名');
+        $grid->email('邮箱');
+        $grid->email_verified('已验证邮箱')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->created_at('注册时间');
+        $grid->disableCreateButton();
+
+        $grid->actions(function ($actions) {
+            // 不在每一行后面展示查看按钮
+            $actions->disableView();
+
+            // 不在每一行后面展示删除按钮
+            $actions->disableDelete();
+
+            // 不在每一行后面展示编辑按钮
+            $actions->disableEdit();
+        });
+
+        $grid->tools(function ($tools) {
+
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('Name');
+        $show->email('Email');
+        $show->password('Password');
+        $show->remember_token('Remember token');
+        $show->email_verified('Email verified');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+}
