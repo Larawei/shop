ad988afc64d4cf8bfc3a68dece659749b24fb015
LaraWei <764098204@qq.com>
2018-11-06 17:08:53 +0800

    集成支付宝

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Log;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            if (app()->environment() !== 'production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            return Pay::alipay($config);
+        });
     }
 }
